0f9ef15fe5d09f16cbd3f4198bd9e9d94c52e6d7
Meghnaapawar <meghnapawar0604@gmail.com>
2026-02-23 14:21:16 +0530

    Updated config for production

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 
+
+
+import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Carlos@55",
-    database="feminova_db",
+    host=os.environ.get("dpg-d6e18h75r7bs73bbfskg-a"),
+    user=os.environ.get("feminova_user"),
+    password=os.environ.get("InNmYo9axkBI2uFZ8qS1vrcRLQ7XpofF"),
+    database=os.environ.get("feminova"),
 )
 
 def get_cursor():
     return db.cursor(dictionary=True, buffered=True)
-
